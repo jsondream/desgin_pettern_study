5ceb0f5c9458fefff274d68f7c445a5a905c4421
wgd12389 <1452506769@qq.com>
2015-06-14 11:55:09 +0800

    工厂模式
    工厂模式

---
+++ src/main/java/com/jsondream/design_pettern/factory/paramFactory/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * <p>Title: package-info.java<／p>
+ * <p>Description: <／p>
+ * <p>Copyright: Copyright (c) 2015<／p>
+ * <p>Company: kingdowin<／p>
+ *
+ * @author    	  <a href="mailto:jsondream@gmail.com">wgd</a>
+ * @date            2015年6月13日
+ * @version  	  1.0
+ */
+package com.jsondream.design_pettern.factory.paramFactory;
